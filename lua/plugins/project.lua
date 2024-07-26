9993c6c661f324480f6943d5e11211b1b0fda5ea
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2024-07-26 09:57:08 +0300

    Projects

---
+++ lua/plugins/project.lua
@@ -0,0 +1,27 @@
+return {
+  "jay-babu/project.nvim",
+  main = "project_nvim",
+  event = "VeryLazy",
+  opts = { ignore_lsp = { "lua_ls" } },
+  specs = {
+    { "AstroNvim/astrocore", opts = { rooter = { enabled = false } } },
+    {
+      "nvim-telescope/telescope.nvim",
+      optional = true,
+      dependencies = { "jay-babu/project.nvim" },
+      opts = function() require("telescope").load_extension "projects" end,
+    },
+  },
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>P"] = { "<Cmd>Telescope projects<CR>", desc = "Open Project panel" },
+          },
+        },
+      },
+    },
+    }
+  }
