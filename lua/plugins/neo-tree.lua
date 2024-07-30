20e17792025b446cef881f98c55e9b57707c3a84
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2024-07-30 12:04:49 +0300

    follow file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,12 @@ return {
 { "nvim-neo-tree/neo-tree.nvim",
     config = function ()
         require("neo-tree").setup({
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = false,
+              },
+            },
             window = {
               mappings = {
                 ["S"] = "split_with_window_picker",
